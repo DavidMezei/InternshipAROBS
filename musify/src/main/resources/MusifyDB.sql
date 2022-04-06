ebd1b18321771143c09f4801393483d18d0f8f60
DavidMezei <davidmezei@hotmail.com>
2022-04-06 19:19:55 +0000

    Change database model

--- musify/src/main/resources/MusifyDB.sql
+++ musify/src/main/resources/MusifyDB.sql
@@ -1,288 +1,124 @@
-CREATE DATABASE  IF NOT EXISTS `musify` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `musify`;
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: musify
--- ------------------------------------------------------
--- Server version	8.0.28
+CREATE TABLE users (
+                       id INT NOT NULL AUTO_INCREMENT,
+                       first_name VARCHAR(45),
+                       last_name VARCHAR(45),
+                       email VARCHAR(45),
+                       encrypted_password VARCHAR(45),
+                       country VARCHAR(45),
+                       role VARCHAR(45),
+                       status VARCHAR(45),
+                       PRIMARY KEY (id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE playlists (
+                           id INT NOT NULL AUTO_INCREMENT,
+                           owner_user_id INT,
+                           type VARCHAR(45),
+                           created_date DATE,
+                           updated_date DATE,
+                           PRIMARY KEY (id),
+                           FOREIGN KEY (owner_user_id)
+                               REFERENCES users(id)
+);
 
---
--- Table structure for table `albums`
---
+CREATE TABLE songs (
+                       id INT NOT NULL AUTO_INCREMENT,
+                       title VARCHAR(45),
+                       duration TIME,
+                       created_date DATE,
+                       PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS `albums`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albums` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `genre` varchar(45) DEFAULT NULL,
-  `release_date` date DEFAULT NULL,
-  `label` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE alternative_song_titles (
+                                         id INT NOT NULL AUTO_INCREMENT,
+                                         song_id INT,
+                                         alternative_title VARCHAR(45),
+                                         PRIMARY KEY (id),
+                                         FOREIGN KEY (song_id)
+                                             REFERENCES songs(id)
+);
 
---
--- Table structure for table `albums_artists`
---
+CREATE TABLE albums (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        title VARCHAR(45),
+                        description VARCHAR(45),
+                        genre VARCHAR(45),
+                        release_date DATE,
+                        label VARCHAR(45),
+                        PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS `albums_artists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albums_artists` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `album_id` int DEFAULT NULL,
-  `artist_id` int DEFAULT NULL,
-  `band_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `album_id` (`album_id`),
-  KEY `artist_id` (`artist_id`),
-  KEY `fk_albums_artists_bands1_idx` (`band_id`),
-  CONSTRAINT `albums_artists_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
-  CONSTRAINT `albums_artists_ibfk_2` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`),
-  CONSTRAINT `fk_albums_artists_bands1` FOREIGN KEY (`band_id`) REFERENCES `bands` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE artists (
+                         id INT NOT NULL AUTO_INCREMENT,
+                         first_name VARCHAR(45),
+                         last_name VARCHAR(45),
+                         stage_name VARCHAR(45),
+                         birthday DATE,
+                         activity_start_date VARCHAR(45),
+                         activity_end_date VARCHAR(45),
+                         PRIMARY KEY (id)
+);
 
---
--- Table structure for table `albums_songs`
---
+CREATE TABLE bands (
+                       id INT NOT NULL AUTO_INCREMENT,
+                       band_name VARCHAR(45),
+                       location VARCHAR(45),
+                       activity_start_date VARCHAR(45),
+                       activity_end_date VARCHAR(45),
+                       PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS `albums_songs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albums_songs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `album_id` int DEFAULT NULL,
-  `song_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `album_id` (`album_id`),
-  KEY `song_id` (`song_id`),
-  CONSTRAINT `albums_songs_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
-  CONSTRAINT `albums_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE playlists_songs (
+                                 @@ -101,37 +91,43 @@ CREATE TABLE users (
+                                 REFERENCES songs(id)
+    );
 
---
--- Table structure for table `alternative_song_titles`
---
+CREATE TABLE artists_albums (
+                                id INT NOT NULL AUTO_INCREMENT,
+                                artist_id INT,
+                                band_id INT,
+                                album_id INT,
+                                PRIMARY KEY (id),
+                                FOREIGN KEY (artist_id)
+                                    REFERENCES artists(id),
+                                FOREIGN KEY (band_id)
+                                    REFERENCES bands(id),
+                                FOREIGN KEY (album_id)
+                                    REFERENCES albums(id)
+);
 
-DROP TABLE IF EXISTS `alternative_song_titles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `alternative_song_titles` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `song_id` int DEFAULT NULL,
-  `alternative_title` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `song_id` (`song_id`),
-  CONSTRAINT `alternative_song_titles_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE artists_songs (
+                               id INT NOT NULL AUTO_INCREMENT,
+                               artist_id INT,
+                               band_id INT,
+                               song_id INT,
+                               PRIMARY KEY (id),
+                               FOREIGN KEY (artist_id)
+                                   REFERENCES artists(id),
+                               FOREIGN KEY (band_id)
+                                   REFERENCES bands(id),
+                               FOREIGN KEY (song_id)
+                                   REFERENCES songs(id)
+);
 
---
--- Table structure for table `artists`
---
+CREATE TABLE bands_artists (
+                               id INT NOT NULL AUTO_INCREMENT,
+                               band_id INT,
+                               artist_id INT,
+                               PRIMARY KEY (id),
+                               FOREIGN KEY (band_id)
+                                   REFERENCES bands(id),
+                               FOREIGN KEY (artist_id)
+                                   REFERENCES artists(id)
+);
 
-DROP TABLE IF EXISTS `artists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `artists` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `activity_start_date` varchar(45) DEFAULT NULL,
-  `activity_end_date` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `stage_name` varchar(45) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `artists_songs`
---
-
-DROP TABLE IF EXISTS `artists_songs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `artists_songs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `artist_id` int DEFAULT NULL,
-  `song_id` int DEFAULT NULL,
-  `band_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `artist_id` (`artist_id`),
-  KEY `song_id` (`song_id`),
-  KEY `fk_artists_songs_bands1_idx` (`band_id`),
-  CONSTRAINT `artists_songs_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`),
-  CONSTRAINT `artists_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`),
-  CONSTRAINT `fk_artists_songs_bands1` FOREIGN KEY (`band_id`) REFERENCES `bands` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bands`
---
-
-DROP TABLE IF EXISTS `bands`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bands` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `artist_id` int DEFAULT NULL,
-  `band_name` varchar(45) DEFAULT NULL,
-  `location` varchar(45) DEFAULT NULL,
-  `activity_start_date` date DEFAULT NULL,
-  `activity_end_date` date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `artist_id` (`artist_id`),
-  CONSTRAINT `bands_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bands_artisits`
---
-
-DROP TABLE IF EXISTS `bands_artisits`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bands_artisits` (
-  `id` int NOT NULL,
-  `artist_id` int DEFAULT NULL,
-  `band_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_bands_artisits_artists1_idx` (`artist_id`),
-  KEY `fk_bands_artisits_bands1_idx` (`band_id`),
-  CONSTRAINT `fk_bands_artisits_artists1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`),
-  CONSTRAINT `fk_bands_artisits_bands1` FOREIGN KEY (`band_id`) REFERENCES `bands` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `playlists`
---
-
-DROP TABLE IF EXISTS `playlists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `playlists` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `owner_user_id` int DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  `created_date` date DEFAULT NULL,
-  `updated_date` date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_user_id` (`owner_user_id`),
-  CONSTRAINT `playlists_ibfk_1` FOREIGN KEY (`owner_user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `playlists_songs`
---
-
-DROP TABLE IF EXISTS `playlists_songs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `playlists_songs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `playlist_id` int DEFAULT NULL,
-  `song_id` int DEFAULT NULL,
-  `song_number` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `playlist_id` (`playlist_id`),
-  KEY `song_id` (`song_id`),
-  CONSTRAINT `playlists_songs_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`id`),
-  CONSTRAINT `playlists_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `songs`
---
-
-DROP TABLE IF EXISTS `songs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `songs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `duration` time DEFAULT NULL,
-  `created_date` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `encrypted_password` varchar(45) NOT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  `role` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users_playlists`
---
-
-DROP TABLE IF EXISTS `users_playlists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_playlists` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `playlist_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `playlist_id` (`playlist_id`),
-  CONSTRAINT `users_playlists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `users_playlists_ibfk_2` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping events for database 'musify'
---
-
---
--- Dumping routines for database 'musify'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-05-03 18:20:07
+CREATE TABLE users_playlists (
+                                 id INT NOT NULL AUTO_INCREMENT,
+                                 user_id INT,
+                                 playlist_id INT,
+                                 PRIMARY KEY (id),
+                                 FOREIGN KEY (user_id)
+                                     REFERENCES users(id),
+                                 FOREIGN KEY (playlist_id)
+                                     REFERENCES playlists(id)
+);
