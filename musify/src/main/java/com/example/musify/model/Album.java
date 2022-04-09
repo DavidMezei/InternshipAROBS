503471e471c2782fa90316ba40db444ba9a988d0
DavidMezei <davidmezei@hotmail.com>
2022-04-09 10:22:14 +0000

    Add new models

---
+++ musify/src/main/java/com/example/musify/model/Album.java
@@ -0,0 +1,69 @@
+package com.example.musify.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "albums")
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String description;
+    private String genre;
+    @Column(name = "release_date")
+    private Date releaseDate;
+    private String label;
+
+    public Album() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
